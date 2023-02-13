f9da8cc5a93c5b68c7443059be5069b2c9c28c60
taztheprogrammer <123741432+taztheprogrammer@users.noreply.github.com>
2023-02-13 12:42:31 -0500

    Add 3 buttons to CalculatorGui
    I also added functionality to the number buttons

--- CalculatorGui.py
+++ CalculatorGui.py
@@ -9,30 +9,41 @@ e = tk.Entry(root)
 e.grid(row=0, column=0, padx=10, pady=5, columnspan=3)
 #e.insert(0, "Enter Your Name:")
 
-def click():
-    pass
+def click(number):
+    e.delete(0, 10)
+    e.insert(0, number)
 
-b1= tk.Button(root, text="1", padx=40, pady=20, command=click)
-b2= tk.Button(root, text="2", padx=40, pady=20, command=click)
-b3= tk.Button(root, text="3", padx=40, pady=20, command=click)
-b4= tk.Button(root, text="4", padx=40, pady=20, command=click)
-b5= tk.Button(root, text="5", padx=40, pady=20, command=click)
-b6= tk.Button(root, text="6", padx=40, pady=20, command=click)
-b7= tk.Button(root, text="7", padx=40, pady=20, command=click)
-b8= tk.Button(root, text="8", padx=40, pady=20, command=click)
-b9= tk.Button(root, text="9", padx=40, pady=20, command=click)
-b0= tk.Button(root, text="0", padx=40, pady=20, command=click)
+b1= tk.Button(root, text="1", padx=40, pady=20, command=lambda: click(1))
+b2= tk.Button(root, text="2", padx=40, pady=20, command=lambda: click(2))
+b3= tk.Button(root, text="3", padx=40, pady=20, command=lambda: click(3))
+b4= tk.Button(root, text="4", padx=40, pady=20, command=lambda: click(4))
+b5= tk.Button(root, text="5", padx=40, pady=20, command=lambda: click(5))
+b6= tk.Button(root, text="6", padx=40, pady=20, command=lambda: click(6))
+b7= tk.Button(root, text="7", padx=40, pady=20, command=lambda: click(7))
+b8= tk.Button(root, text="8", padx=40, pady=20, command=lambda: click(8))
+b9= tk.Button(root, text="9", padx=40, pady=20, command=lambda: click(9))
+b0= tk.Button(root, text="0", padx=40, pady=20, command=lambda: click(0))
+b_add= tk.Button(root, text="+", padx=40, pady=20, command=click)
+b_clear= tk.Button(root, text="c", padx=40, pady=20, command=click)
+b_equal= tk.Button(root, text="=", padx=40, pady=20, command=click)
 
 b1.grid(row=4, column=0)
 b2.grid(row=4, column=1)
 b3.grid(row=4, column=2)
+
 b4.grid(row=3, column=0)
 b5.grid(row=3, column=1)
 b6.grid(row=3, column=2)
+
 b7.grid(row=2, column=0)
 b8.grid(row=2, column=1)
 b9.grid(row=2, column=2)
+
 b0.grid(row=5, column=0)
+b_add.grid(row=5, column=1)
+b_clear.grid(row=5, column=2)
+
+b_equal.grid(row=6, column=0)
 
 
 
